6477a73c41e0a711e3c8d135450f4ce55eaaa975
haolq664 <lehao99nd@gmail.com>
2026-06-30 17:42:29 +0700

    lap1

--- fe2-web2091-summer26-main/src/App.tsx
+++ fe2-web2091-summer26-main/src/App.tsx
@@ -1,7 +1,28 @@
-import { Toaster } from "react-hot-toast";
-import { Link } from "react-router-dom";
-
+import { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import { Layout } from 'antd';
+import { Form, Input, Button } from 'antd';
+import { Table } from 'antd';
+import { Modal } from 'antd';
+import { useState } from 'react';
 function App() {
+  const { Header, Content, Footer, Sider } = Layout;
+  const onFinish = (values: any) => {
+    console.log(values);
+  };
+  const columns = [
+    { title: 'Name', dataIndex: 'name' },
+    { title: 'Email', dataIndex: 'email' },
+    { title: 'Role', dataIndex: 'role' },
+  ];
+
+  const data = [
+    { key: 1, name: 'John', email: 'lehao123@gmail.com', role: 'admin' },
+    { key: 2, name: 'Anna', email: 'lehao123@gmail.com', role: 'user' },
+  ];
+  const [open, setOpen] = useState(false);
+  //lap 1
+
   return (
     <>
       <nav className="bg-blue-600 text-white shadow">
@@ -36,8 +57,100 @@ function App() {
       {/* MAIN CONTENT */}
       <div className="max-w-6xl mx-auto mt-10 px-4 text-center">
         <h1 className="text-4xl font-bold mb-4">Chào mừng đến với WEB2091</h1>
-      </div>
+        <Button>Login</Button>
+        <Button type="primary">Login</Button>
+        <Button type="text">text</Button>
+        <Button type="link">Link</Button>
+        <Layout>
+          <Header style={{ color: 'white' }}>Header</Header>
+          <Content style={{ padding: 20 }}>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+        <Form onFinish={onFinish}>
+          <label htmlFor="">Nhập email</label>
+          <Form.Item name="email" rules={[{ required: true, message: 'Nhập email' }]}>
+            <Input placeholder="Email" />
+          </Form.Item>
+          <label htmlFor="">Nhập password</label>
+          <Form.Item name="password" rules={[{ required: true, message: 'Nhập password' }]}>
+            <Input placeholder="Password" />
+          </Form.Item>
 
+          <Form.Item>
+            <Button htmlType="submit" type="primary">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+        <Table columns={columns} dataSource={data} />
+        <>
+          <Button onClick={() => setOpen(true)}>Open</Button>
+
+          <Modal open={open} onCancel={() => setOpen(false)} onOk={() => setOpen(false)}>
+            Nội dung modal
+          </Modal>
+        </>
+      </div>
+      {/* lap1 */}
+      <Layout>
+        <Header style={{ color: '#fff' }}>Header</Header>
+        <Layout>
+          <Sider width={220} style={{ color: '#fff' }}>
+            Sidebar
+          </Sider>
+          <Content style={{ padding: 20 }}>Content</Content>
+        </Layout>
+      </Layout>
+
+      <Form onFinish={onFinish}>
+        <label htmlFor="">Nhập Name</label>
+        <Form.Item name="name" rules={[{ required: true, message: 'Nhập email' }]}>
+          <Input placeholder="name" />
+        </Form.Item>
+        <label htmlFor="">Nhập email</label>
+        <Form.Item name="email" rules={[{ required: true, message: 'Nhập email' }]}>
+          <Input placeholder="Email" />
+        </Form.Item>
+        <label htmlFor="">Nhập password</label>
+        <Form.Item name="password" rules={[{ required: true, message: 'Nhập password' }]}>
+          <Input placeholder="Password" />
+        </Form.Item>
+
+        <Form.Item>
+          <Button htmlType="submit" type="primary">
+            Register
+          </Button>
+        </Form.Item>
+      </Form>
+
+      <Table columns={columns} dataSource={data} />
+
+      <>
+        <Button onClick={() => setOpen(true)}>AddUser</Button>
+
+        <Modal open={open} onCancel={() => setOpen(false)} onOk={() => setOpen(false)}>
+          <Form onFinish={onFinish}>
+            <label htmlFor="">Name</label>
+            <Form.Item name="name" rules={[{ required: true, message: 'Nhập email' }]}>
+              <Input placeholder="name" />
+            </Form.Item>
+            <label htmlFor="">Age</label>
+            <Form.Item name="Age" rules={[{ required: true, message: 'Nhập tuoi' }]}>
+              <Input placeholder="Age" />
+            </Form.Item>
+            <label htmlFor="">Email</label>
+            <Form.Item name="Email" rules={[{ required: true, message: 'Nhập email' }]}>
+              <Input placeholder="Email" />
+            </Form.Item>
+
+            <Form.Item>
+              <Button htmlType="submit" type="primary">
+                Save
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
+      </>
       <Toaster />
     </>
   );
